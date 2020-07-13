244fbd9554ea5cfee393ab3b8f86806ff154fdc5
Shashank <svivek2@slb.com>
2020-07-13 18:53:23 +0530

    code refactor

--- pkg/entities/entities.go
+++ pkg/entities/entities.go
@@ -1,7 +1,6 @@
 package entities
 
 import (
-	"e-food/models"
 	"time"
 )
 
@@ -10,21 +9,4 @@ type CouponEntity struct {
 	ExpiryDate time.Time
 	Rule       Rule
 	UserLimit  int
-}
-
-type Rule struct {
-	RuleId   string             `json:"ruleId"`
-	Discount float64            `json:"discount,omitempty"`
-	RuleSet  map[string]*Filter `json:"filters"`
-}
-
-type Filter struct {
-	MinQuantity *int64 `json:"minQuantity,omitempty"`
-	EqQunatity  *int64 `json:"eqQunatity,omitempty"`
-}
-
-type EngineTempItem struct {
-	NumberOfGroupInSet     int
-	TempItem               *models.CartItem
-	ToBeReducedQtyFromCart int64
 }
